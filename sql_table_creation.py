bcd71e9cd372713595832cb97a14f5d047f1307e
Chinchilla7 <regina.chen@stonybrook.edu>
2022-10-19 21:30:31 -0400

    Update sql_table_creation.py

--- sql_table_creation.py
+++ sql_table_creation.py
@@ -3,24 +3,24 @@ from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import dbm
 
 
+#### drop the old tables that do not start with 
+def droppingFunction_limited(dbList, db_source):
+    for table in dbList:
+        if table.startswith('') == False:
+            db_source.execute(f'drop table {table}')
+            print(f'dropped table {table}')
+        else:
+            print(f'kept table {table}') 
 
-#### drop the old tables that do not start with production_
-#def droppingFunction_limited(dbList, db_source):
-  #  for table in dbList:
-   #     if table.startswith('production_') == False:
-   #         db_source.execute(f'drop table {table}')
-    #        print(f'dropped table {table}')
-     #   else:
-      #      print(f'kept table {table}')
-
-#def droppingFunction_all(dbList, db_source):
-   # for table in dbList:
-   #     db_source.execute(f'drop table {table}')
-   #     print(f'dropped table {table}')
-   # else:
-   #     print(f'kept table {table}')
+def droppingFunction_all(dbList, db_source):
+    for table in dbList:
+        db_source.execute(f'drop table {table}')
+        print(f'dropped table {table}')
+    else:
+        print(f'kept table {table}')
 
 #connect to mysql database using credentials
 
@@ -44,19 +44,19 @@ print (db.table_names())
 
 ## ### delete everything 
 #droppingFunction_all(tableNames, db)
+
 #creating new tables within patient_portal database called patients, medications, treatments_procedures, 
 #conditions, and social determinants.  
-#TABLENAME = MYSQL_USER + 'fakeTableAssignment1'
 
 create_Patients_table = """
 create table IF NOT EXISTS patients (
-  id int,
-  mrn varchar(255),
-  first_name varchar(255),
-  last_name varchar(255),
-  zip_code varchar(255),
-  dob varchar(255),
-  gender varchar(255),
+  id int auto_increment,
+  mrn varchar(255) default null unique,
+  first_name varchar(255) default null,
+  last_name varchar(255) default null,
+  zip_code varchar(255) default null,
+  dob varchar(255) default null,
+  gender varchar(255) default null,
   PRIMARY KEY (id),
 );
 """
@@ -65,10 +65,9 @@ db.execute(create_Patients_table)
 
 create_medications_table= """
 create table IF NOT EXISTS medications (
-  id int,
-  mrn varchar(255),
-  medication_id varchar(255),
-  ndc_codes varchar(255),
+  id int auto_increment,
+  medication_name varchar(255) default null,
+  ndc_codes varchar(255) default null unique,
   PRIMARY KEY (id),
   );
   """
@@ -76,10 +75,9 @@ db.execute(create_medications_table)
 
 create_conditions_table= """
 create table IF NOT EXISTS conditions (
-  id int,
-  mrn varchar(255),
-  condition_id varchar(255),
-  icd_10_codes varchar(255),
+  id int auto_increment,
+  icd_10_codes varchar(255) default null unique,
+  icd_description varchar(255) default null,
   PRIMARY KEY (id),
   );
   """
@@ -87,10 +85,9 @@ db.execute(create_conditions_table)
 
 create_treatments_procedures_table= """
 create table IF NOT EXISTS treatments_procedures (
-  id int,
-  mrn varchar(255),
-  treatments_procedures_id varchar(255),
-  cpt_codes varchar(255),
+  id int auto_increment,
+  treatments_procedures_desciption varchar(255),
+  cpt_codes varchar(255) default null unique,
   PRIMARY KEY (id),
   );
   """
@@ -98,17 +95,37 @@ db.execute(create_treatments_procedures_table)
 
 create_social_determinants_table= """
 create table IF NOT EXISTS social_determinants (
-  id int,
-  mrn varchar(255),
-  social_determinants_id varchar(255),
-  loinc_codes varchar(255),
+  id int auto_increment,
+  social_determinants_description varchar(255),
+  loinc_codes varchar(255) default null unique,
   PRIMARY KEY (id),
   );
   """
 db.execute(create_social_determinants_table)
 
+create_Patients_medications = """
+create table IF NOT EXISTS patients_medication (
+  id int auto_increment,
+  mrn varchar(255) default null,
+  ndc_codes varchar(255) default null,
+  PRIMARY KEY (id),
+  FOREIGN KEY (mrn) REFERNCES patients(mrn) ON DELETE CASCADE,
+  FOREIGN KEY (ndc_codes) REFERNCES medications(ndc_codes) ON DELETE CASCADE,
+);
+"""
+db.execute(create_Patients_medications)
 
-
+create_Patients_conditions = """
+create table IF NOT EXISTS patients_conditions (
+  id int auto_increment,
+  mrn varchar(255) default null,
+  icd_10_codes varchar(255) default null,
+  PRIMARY KEY (id),
+  FOREIGN KEY (mrn) REFERNCES patients(mrn) ON DELETE CASCADE,
+  FOREIGN KEY (icd_10_codes) REFERNCES medications(icd_10_codes) ON DELETE CASCADE,
+);
+"""
+db.execute(create_Patients_conditions)
 
 
 
